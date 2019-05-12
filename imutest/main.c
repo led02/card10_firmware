9de1261b2d9cfa16f0546726bd15046039a729f1
schneider <schneider@blinkenlichts.net>
2019-05-12 11:33:04 +0200

    feat(imu): Add testapp

---
+++ imutest/main.c
@@ -0,0 +1,324 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Maxim Integrated Products, Inc., All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of Maxim Integrated
+ * Products, Inc. shall not be used except as stated in the Maxim Integrated
+ * Products, Inc. Branding Policy.
+ *
+ * The mere transfer of this software does not imply any licenses
+ * of trade secrets, proprietary technology, copyrights, patents,
+ * trademarks, maskwork rights, or any other form of intellectual
+ * property whatsoever. Maxim Integrated Products, Inc. retains all
+ * ownership rights.
+ *
+ * $Date: 2018-09-04 22:13:32 +0000 (Tue, 04 Sep 2018) $
+ * $Revision: 37649 $
+ *
+ ******************************************************************************/
+
+/**
+ * @file    main.c
+ * @brief   Hello World!
+ * @details This example uses the UART to print to a terminal and flashes an LED.
+ */
+
+/***** Includes *****/
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "mxc_config.h"
+#include "led.h"
+#include "board.h"
+#include "tmr_utils.h"
+#include "i2c.h"
+#include "rtc.h"
+#include "spi.h"
+#include "gpio.h"
+#include "oled96.h"
+#include "bhy.h"
+#include "Bosch_PCB_7183_di03_BMI160_BMM150-7183_di03.2.1.11696_170103.h"
+#include "bhy_uc_driver.h"
+#include "pmic.h"
+#include <math.h>
+
+/***** Definitions *****/
+
+#define I2C_DEVICE	    MXC_I2C0_BUS0
+
+#define SPI SPI0
+#define SPI_SPEED       1000000  // Bit Rate
+
+/* should be greater or equal to 69 bytes, page size (50) + maximum packet size(18) + 1 */
+#define FIFO_SIZE                      300
+#define ROTATION_VECTOR_SAMPLE_RATE    100
+#define MAX_PACKET_LENGTH              18
+#define OUT_BUFFER_SIZE                60
+
+
+/***** Globals *****/
+static const gpio_cfg_t motor_pin = {PORT_0, PIN_8, GPIO_FUNC_OUT, GPIO_PAD_NONE};
+char out_buffer[OUT_BUFFER_SIZE] = " W: 0.999  X: 0.999  Y: 0.999  Z: 0.999   \r";
+uint8_t fifo[FIFO_SIZE];
+
+
+
+/***** Functions *****/
+#if 0
+void I2C0_IRQHandler(void)
+{
+    I2C_Handler(I2C_DEVICE);
+    return;
+}
+#endif
+
+uint32_t ecg_read_reg(uint8_t reg)
+{
+    spi_req_t req;
+    uint8_t tx_data[] = {(reg << 1) | 1, 0, 0, 0};
+    uint8_t rx_data[] = {0, 0, 0, 0};
+    req.tx_data = tx_data;
+    req.rx_data = rx_data;
+    req.len = 4;
+    req.bits = 8;
+    req.width = SPI17Y_WIDTH_1;
+    req.ssel = 0;
+    req.deass = 1;
+    req.ssel_pol = SPI17Y_POL_LOW;
+    req.tx_num = 0;
+    req.rx_num = 0;
+
+    SPI_MasterTrans(SPI, &req);
+
+    return (rx_data[1] << 16) | (rx_data[2] << 8) | rx_data[3];
+}
+
+static void sensors_callback_vector(bhy_data_generic_t * sensor_data, bhy_virtual_sensor_t sensor_id)
+{
+    printf("x=%d, y=%d, z=%d status=%d\n",
+    sensor_data->data_vector.x,
+    sensor_data->data_vector.y,
+    sensor_data->data_vector.z,
+    sensor_data->data_vector.status
+    );
+}
+
+/*!
+ * @brief This function is  callback function for acquring sensor datas
+ *
+ * @param[in]   sensor_data
+ * @param[in]   sensor_id
+ */
+static void sensors_callback_rotation_vector(bhy_data_generic_t * sensor_data, bhy_virtual_sensor_t sensor_id)
+{
+#if 0
+    float temp;
+    uint8_t index;
+
+    temp = sensor_data->data_quaternion.w / 16384.0f; /* change the data unit by dividing 16384 */
+    out_buffer[3] = temp < 0 ? '-' : ' ';
+    temp = temp < 0 ? -temp : temp;
+    out_buffer[4] = floorf(temp) + '0';
+
+    for (index = 6; index <= 8; index++)
+    {
+        temp = (temp - floorf(temp)) * 10;
+        out_buffer[index] = floorf(temp) + '0';
+    }
+
+    temp = sensor_data->data_quaternion.x / 16384.0f;
+    out_buffer[13] = temp < 0 ? '-' : ' ';
+    temp = temp < 0 ? -temp : temp;
+    out_buffer[14] = floorf(temp) + '0';
+
+    for (index = 16; index <= 18; index++)
+    {
+        temp = (temp - floorf(temp)) * 10;
+        out_buffer[index] = floorf(temp) + '0';
+    }
+
+    temp = sensor_data->data_quaternion.y / 16384.0f;
+    out_buffer[23] = temp < 0 ? '-' : ' ';
+    temp = temp < 0 ? -temp : temp;
+    out_buffer[24] = floorf(temp) + '0';
+
+    for (index = 26; index <= 28; index++)
+    {
+        temp = (temp - floorf(temp)) * 10;
+        out_buffer[index] = floorf(temp) + '0';
+    }
+
+    temp = sensor_data->data_quaternion.z / 16384.0f;
+    out_buffer[33] = temp < 0 ? '-' : ' ';
+    temp = temp < 0 ? -temp : temp;
+    out_buffer[34] = floorf(temp) + '0';
+
+    for (index = 36; index <= 38; index++)
+    {
+        temp = (temp - floorf(temp)) * 10;
+        out_buffer[index] = floorf(temp) + '0';
+    }
+#endif
+
+    printf("x=%d, y=%d, z=%d, w=%d\n",
+    sensor_data->data_quaternion.x,
+    sensor_data->data_quaternion.y,
+    sensor_data->data_quaternion.z,
+    sensor_data->data_quaternion.w
+    );
+}
+
+// *****************************************************************************
+int main(void)
+{
+    printf("Hello World!\n");
+    TMR_Delay(MXC_TMR0, MSEC(1000), 0);
+
+    //Setup the I2CM
+    I2C_Shutdown(MXC_I2C0_BUS0);
+    I2C_Init(MXC_I2C0_BUS0, I2C_FAST_MODE, NULL);
+
+    I2C_Shutdown(MXC_I2C1_BUS0);
+    I2C_Init(MXC_I2C1_BUS0, I2C_FAST_MODE, NULL);
+
+ #if 0
+    NVIC_EnableIRQ(I2C0_IRQn); // Not sure if we actually need this when not doing async requests
+ #endif
+
+    uint8_t dummy[1] = {0};
+    // "7-bit addresses 0b0000xxx and 0b1111xxx are reserved"
+    for (int addr = 0x8; addr < 0x78; ++addr) {
+        // A 0 byte write does not seem to work so always send a single byte.
+        int res = I2C_MasterWrite(MXC_I2C0_BUS0, addr << 1, dummy, 1, 0);
+        if(res == 1) {
+            printf("Found (7 bit) address 0x%02x on I2C0\n", addr);
+        }
+
+        res = I2C_MasterWrite(MXC_I2C1_BUS0, addr << 1, dummy, 1, 0);
+        if(res == 1) {
+            printf("Found (7 bit) address 0x%02x on I2C1\n", addr);
+        }
+    }
+
+    pmic_init();
+    pmic_set_led(0, 0);
+    pmic_set_led(1, 0);
+    pmic_set_led(2, 0);
+
+    TMR_Delay(MXC_TMR0, MSEC(1000), 0);
+
+#if 0
+    oledInit(0x3c, 0, 0);
+    oledFill(0x00);
+    oledWriteString(0, 0, "Hello", 0);
+    oledWriteString(0, 2, "my name is", 0);
+    oledWriteString(0, 4, "card10", 1);
+#endif
+
+    /* BHY Variable*/
+    uint8_t                    *fifoptr           = NULL;
+    uint8_t                    bytes_left_in_fifo = 0;
+    uint16_t                   bytes_remaining    = 0;
+    uint16_t                   bytes_read         = 0;
+    bhy_data_generic_t         fifo_packet;
+    bhy_data_type_t            packet_type;
+    BHY_RETURN_FUNCTION_TYPE   result;
+
+    const gpio_cfg_t interrupt_pin = {PORT_0, PIN_13, GPIO_FUNC_IN, GPIO_PAD_PULL_UP};
+    GPIO_Config(&interrupt_pin);
+
+    if(bhy_driver_init(bhy1_fw)) {
+        printf("Failed to init bhy\n");
+    }
+
+    /* wait for the bhy trigger the interrupt pin go down and up again */
+    while (GPIO_InGet(&interrupt_pin));
+
+    while (!GPIO_InGet(&interrupt_pin));
+
+#if 0
+    /* install the callback function for parse fifo data */
+    if(bhy_install_sensor_callback(VS_TYPE_ROTATION_VECTOR, VS_WAKEUP, sensors_callback_rotation_vector))
+    {
+        printf("Fail to install sensor callback\n");
+    }
+#endif
+
+    //if(bhy_install_sensor_callback(VS_TYPE_GEOMAGNETIC_FIELD, VS_WAKEUP, sensors_callback_vector))
+    if(bhy_install_sensor_callback(VS_TYPE_GRAVITY, VS_WAKEUP, sensors_callback_vector))
+    {
+        printf("Fail to install sensor callback\n");
+    }
+
+#if 0
+    /* enables the virtual sensor */
+    if(bhy_enable_virtual_sensor(VS_TYPE_ROTATION_VECTOR, VS_WAKEUP, ROTATION_VECTOR_SAMPLE_RATE, 0, VS_FLUSH_NONE, 0, 0))
+    {
+        printf("Fail to enable sensor id=%d\n", VS_TYPE_ROTATION_VECTOR);
+    }
+#endif
+
+    /* enables the virtual sensor */
+    //if(bhy_enable_virtual_sensor(VS_TYPE_GEOMAGNETIC_FIELD, VS_WAKEUP, ROTATION_VECTOR_SAMPLE_RATE, 0, VS_FLUSH_NONE, 0, 0))
+    if(bhy_enable_virtual_sensor(VS_TYPE_GRAVITY, VS_WAKEUP, ROTATION_VECTOR_SAMPLE_RATE, 0, VS_FLUSH_NONE, 0, 0))
+    {
+        printf("Fail to enable sensor id=%d\n", VS_TYPE_GEOMAGNETIC_FIELD);
+    }
+
+
+    while(1)
+    {
+        /* wait until the interrupt fires */
+        /* unless we already know there are bytes remaining in the fifo */
+        while (!GPIO_InGet(&interrupt_pin) && !bytes_remaining);
+
+        bhy_read_fifo(fifo + bytes_left_in_fifo, FIFO_SIZE - bytes_left_in_fifo, &bytes_read, &bytes_remaining);
+        bytes_read           += bytes_left_in_fifo;
+        fifoptr              = fifo;
+        packet_type          = BHY_DATA_TYPE_PADDING;
+
+        do
+        {
+            /* this function will call callbacks that are registered */
+            result = bhy_parse_next_fifo_packet(&fifoptr, &bytes_read, &fifo_packet, &packet_type);
+
+            /* prints all the debug packets */
+            if (packet_type == BHY_DATA_TYPE_DEBUG)
+            {
+                bhy_print_debug_packet(&fifo_packet.data_debug, bhy_printf);
+            }
+
+            /* the logic here is that if doing a partial parsing of the fifo, then we should not parse  */
+            /* the last 18 bytes (max length of a packet) so that we don't try to parse an incomplete   */
+            /* packet */
+        } while ((result == BHY_SUCCESS) && (bytes_read > (bytes_remaining ? MAX_PACKET_LENGTH : 0)));
+
+        bytes_left_in_fifo = 0;
+
+        if (bytes_remaining)
+        {
+            /* shifts the remaining bytes to the beginning of the buffer */
+            while (bytes_left_in_fifo < bytes_read)
+            {
+                fifo[bytes_left_in_fifo++] = *(fifoptr++);
+            }
+        }
+    }
+}
