4c04057e493d661e2385d2220a771fa96a08e635
Rahix <rahix@rahix.de>
2019-06-13 21:32:47 +0200

    feat(api-demo): Add dual-core stuff

---
+++ hw-tests/api-demo/flash.gdb
@@ -0,0 +1,13 @@
+source ../../.gdbinit
+
+set confirm off
+
+echo Loading core1 image ...
+file ../../build/hw-tests/api-demo/api-demo-core1.elf
+load
+echo Loading core0 image ...
+file ../../build/hw-tests/api-demo/api-demo-core0.elf
+load
+
+reset
+quit
--- hw-tests/api-demo/main.c
+++ hw-tests/api-demo/main.c
@@ -5,6 +5,16 @@
 #include "card10.h"
 #include "tmr_utils.h"
 
+void Core1_Start(void) {
+    //MXC_GCR->gp0 = (uint32_t)(&__isr_vector_core1);
+    MXC_GCR->gp0 = 0x10040000;
+    MXC_GCR->perckcn1 &= ~MXC_F_GCR_PERCKCN1_CPU1;
+}
+
+void Core1_Stop(void) {
+    MXC_GCR->perckcn1 |= MXC_F_GCR_PERCKCN1_CPU1;
+}
+
 int main(void)
 {
     int count = 0;
