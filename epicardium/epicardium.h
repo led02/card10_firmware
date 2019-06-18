0acc3fe596b8f38e6671fd72ba57502673c80b57
ch3 <ch3@mailbox.org>
2019-06-18 23:14:58 +0200

    feat(epicardium): Add leds_set function to API

--- epicardium/epicardium.h
+++ epicardium/epicardium.h
@@ -12,4 +12,7 @@ API(API_UART_WRITE, void epic_uart_write_str(char*str, intptr_t length));
 #define API_UART_READ  0x2
 API(API_UART_READ, char epic_uart_read_chr(void));
 
+#define API_LEDS_SET  0x3
+API(API_LEDS_SET, void epic_leds_set(int led, uint8_t r, uint8_t g, uint8_t b));
+
 #endif /* _EPICARDIUM_H */
