9b2cf5cafd65ee8a6f99f916e2c10bfe0fafa86a
Rahix <rahix@rahix.de>
2019-07-11 11:53:25 +0200

    feat(pycardium): Enable ure regular expressions module
    Signed-off-by: Rahix <rahix@rahix.de>

--- pycardium/mpconfigport.h
+++ pycardium/mpconfigport.h
@@ -25,6 +25,10 @@
 #define MICROPY_PY_UJSON                    (1)
 #define MICROPY_PY_URANDOM                  (1)
 #define MICROPY_PY_URANDOM_EXTRA_FUNCS      (1)
+#define MICROPY_PY_URE                      (1)
+#define MICROPY_PY_URE_MATCH_GROUPS         (1)
+#define MICROPY_PY_URE_MATCH_SPAN_START_END (1)
+#define MICROPY_PY_URE_SUB                  (1)
 #define MICROPY_PY_UTIME_MP_HAL             (1)
 
 /* Modules */
