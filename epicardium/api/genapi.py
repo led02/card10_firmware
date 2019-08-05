bd22615920bfdbcf15a4282dc4d638169aa8c853
Rahix <rahix@rahix.de>
2019-08-05 21:56:38 +0200

    fix(genapi): Fix preprocessor interfering with code-gen
    Signed-off-by: Rahix <rahix@rahix.de>

--- epicardium/api/genapi.py
+++ epicardium/api/genapi.py
@@ -124,7 +124,7 @@ def main():
 
     # Evaluate the preprocessor
     source = subprocess.check_output(
-        ["gcc", "-E", "-"], input=api_src.encode()
+        ["gcc", "-E", "-P", "-"], input=api_src.encode()
     ).decode()
 
     declarations = parse_declarations(source)
