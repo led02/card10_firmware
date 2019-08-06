3fb21e97170a23be75e0522aebaa9615a9101eb4
Sergiusz Bazanski <q3k@q3k.org>
2019-08-06 22:29:53 +0200

    ci: move images to dockerhub
    k0.hswaw.net has, uh, issues now.

--- docker/build-env/push.sh
+++ docker/build-env/push.sh
@@ -8,7 +8,7 @@ function rev() {
     ( cd $1; git describe --always --match "v[0-9].*" --dirty )
 }
 
-IMAGE=registry.k0.hswaw.net/q3k/card10-build-env
+IMAGE=derq3k/card10-build-env
 TAG=$(TZ=UTC date +%Y%m%d-%H%M%SZ)-$(rev .)
 
 docker build -t $IMAGE:$TAG .
--- docker/deploy-env/push.sh
+++ docker/deploy-env/push.sh
@@ -8,7 +8,7 @@ function rev() {
     ( cd $1; git describe --always --match "v[0-9].*" --dirty )
 }
 
-IMAGE=registry.k0.hswaw.net/q3k/card10-deploy-env
+IMAGE=derq3k/card10-deploy-env
 TAG=$(TZ=UTC date +%Y%m%d-%H%M%SZ)-$(rev .)
 
 docker build -t $IMAGE:$TAG .
