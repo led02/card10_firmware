92774919c80033d20826a091a047dfbcc8f799af
Rahix <rahix@rahix.de>
2019-08-12 18:33:36 +0200

    fix(leds): Add back missing LED IDs
    Signed-off-by: Rahix <rahix@rahix.de>

--- pycardium/modules/py/leds.py
+++ pycardium/modules/py/leds.py
@@ -1,6 +1,15 @@
 import sys_leds
 import math
 
+BOTTOM_RIGHT = 11
+""" ID of the LED in the bottom right corner. """
+BOTTOM_LEFT = 12
+""" ID of the LED in the bottom left corner. """
+TOP_RIGHT = 13
+""" ID of the LED in the top right corner. """
+TOP_LEFT = 14
+""" ID of the LED in the top left corner. """
+
 
 def update():
     """
