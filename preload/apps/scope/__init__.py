8e8d86147399b917d6955addff323a257f372fcb
schneider <schneider@blinkenlichts.net>
2019-08-21 18:15:52 +0200

    feat(apps): Add scope to preload

---
+++ preload/apps/scope/__init__.py
@@ -0,0 +1,35 @@
+import os
+import display
+import utime
+import buttons
+import light_sensor
+import math
+
+WIDTH=160
+HEIGHT=80
+
+disp = display.open()
+
+light_sensor.start()
+
+history = []
+
+while True:
+    disp.clear()
+    
+    value = light_sensor.get_reading()
+    
+    history.insert(0, value)
+    if len(history) > WIDTH:
+        history.pop()
+
+    disp.print("%i"%value)
+    
+    for i in range(0,len(history)):
+        # Rescale to range 0 <= value < HEIGHT-1
+        y = math.floor(history[i]*(HEIGHT-2)/max(history))
+        
+        disp.pixel(WIDTH-i, HEIGHT-y-1)
+    
+    disp.update()
+    utime.sleep(0.1)
