083da6e4b2250568bfa3d4591a35405854b0fd21
Pete <pe-card10-gitlab@hindenburgring.com>
2019-08-24 11:23:46 +0000

    Fixed locale environment variables for macOS

--- bootloader/build_multi_image.sh
+++ bootloader/build_multi_image.sh
@@ -6,7 +6,7 @@ BIN1="$2"
 BIN2="$3"
 BINOUT="$4"
 
-dd if=/dev/zero ibs=1k count=448 2>/dev/null | LANG=C LC_CTYPE=C tr "\000" "\377" > "$BINOUT"
+dd if=/dev/zero ibs=1k count=448 2>/dev/null | LANG=C LC_CTYPE=C LC_ALL=C LC_COLLATE=C tr "\000" "\377" > "$BINOUT"
 dd if="$BIN1" of="$BINOUT" conv=notrunc 2>/dev/null
 dd if="$BIN2" >> "$BINOUT" 2>/dev/null
 
