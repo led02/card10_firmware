3b79a7715b655a54d013572bdd4e2001c473056d
Hauke Mehrtens <hauke@hauke-m.de>
2019-08-24 11:10:23 +0200

    BLE: FileTrans: Require encryption
    Require encryption for the file transfer service.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- epicardium/ble/filetransfer.c
+++ epicardium/ble/filetransfer.c
@@ -143,7 +143,8 @@ static const attsAttr_t fileTransCfgList[] = {
 		.pLen        = NULL,
 		.maxLen      = 128,
 		.settings    = ATTS_SET_WRITE_CBACK | ATTS_SET_VARIABLE_LEN,
-		.permissions = ATTS_PERMIT_WRITE | ATTS_PERMIT_WRITE_AUTH,
+		.permissions = ATTS_PERMIT_WRITE | ATTS_PERMIT_WRITE_ENC |
+			       ATTS_PERMIT_WRITE_AUTH,
 	},
 	/* File transfer Central RX characteristic */
 	{
@@ -161,7 +162,8 @@ static const attsAttr_t fileTransCfgList[] = {
 		.pLen        = &attRxChConfigValue_len,
 		.maxLen      = sizeof(attRxChConfigValue),
 		.settings    = ATTS_SET_VARIABLE_LEN,
-		.permissions = ATTS_PERMIT_READ | ATTS_PERMIT_READ_AUTH,
+		.permissions = ATTS_PERMIT_READ | ATTS_PERMIT_READ_ENC |
+			       ATTS_PERMIT_READ_AUTH,
 	},
 	/* File transfer Central RX notification channel */
 	{
@@ -170,8 +172,9 @@ static const attsAttr_t fileTransCfgList[] = {
 		.pLen        = &attRxChConfigValue_len,
 		.maxLen      = sizeof(attRxChConfigValue),
 		.settings    = ATTS_SET_CCC,
-		.permissions = ATTS_PERMIT_READ | ATTS_PERMIT_READ_AUTH |
-			       ATTS_PERMIT_WRITE | ATTS_PERMIT_WRITE_AUTH,
+		.permissions = ATTS_PERMIT_READ | ATTS_PERMIT_READ_ENC |
+			       ATTS_PERMIT_READ_AUTH | ATTS_PERMIT_WRITE |
+			       ATTS_PERMIT_WRITE_ENC | ATTS_PERMIT_WRITE_AUTH,
 	},
 };
 
