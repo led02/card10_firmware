58acc8032a8f29b1c0c6323d8385bf98cbb7a7a7
schneider <schneider@blinkenlichts.net>
2019-09-15 01:36:01 +0200

    change(vibra): Use a static timer
    Closes #50

--- epicardium/modules/vibra.c
+++ epicardium/modules/vibra.c
@@ -6,6 +6,7 @@ static const gpio_cfg_t motor_pin = {
 	PORT_0, PIN_8, GPIO_FUNC_OUT, GPIO_PAD_NONE
 };
 
+static StaticTimer_t vibra_timer_data;
 static TimerHandle_t vibra_timer = NULL;
 
 void epic_vibra_set(int status)
@@ -27,8 +28,13 @@ void epic_vibra_vibrate(int millis)
 	int ticks = millis * (configTICK_RATE_HZ / 1000);
 
 	if (vibra_timer == NULL) {
-		vibra_timer = xTimerCreate(
-			"vibratimer", ticks, pdFALSE, 0, vTimerCallback
+		vibra_timer = xTimerCreateStatic(
+			"vibratimer",
+			ticks,
+			pdFALSE,
+			0,
+			vTimerCallback,
+			&vibra_timer_data
 		);
 	}
 	if (vibra_timer != NULL) {
